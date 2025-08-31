89c61e95ea9071b6ec9970e4657c2cf86badea8d
Michelle <mpuschkarow@web.de>
2025-08-31 12:48:23 +0200

    logo and favicon added

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import Card from './components/Card.vue';
 import Header from './components/Header.vue';
 import { onMounted } from "vue";
 import { stockStore } from "./stores/stockStore";
+import logo from './assets/images/ms-logo.png'
 
 const stockStoreService = stockStore();
 onMounted(async () => {
@@ -13,8 +14,12 @@ onMounted(async () => {
 </script>
 <template>
   <Header />
-  <main>Hallo</main>
-  <Card />
+  <main>
+    Hallo
+    <Card />
+    <img :src="logo" alt="Logo of Magnificent Seven">
+  </main>
+
 </template>
 
 <style>
@@ -22,5 +27,22 @@ onMounted(async () => {
   width: 100%;
   max-width: 1440px;
   height: 100dvh;
+}
+
+main {
+  height: 100dvh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+
+  img {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    opacity: 0.2;
+    width: 50%;
+    height: auto;
+  }
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,36 @@
 <template>
   <header>
+    <div class="header-flag"></div>
     <h1>The Magnificent Seven Companies</h1>
   </header>
 </template>
 
 <script>
+
 export default {
   name: 'Header',
 }
 </script>
 
 <style scoped>
+  header {
+    display: flex;
+    align-items: center;
+    gap: 16px;
+    padding: 24px 0;
+  }
 
+  h1 {
+    text-align: start;
+    margin: 0;
+    color: white;
+    font-size: 32px;
+  }
+
+  .header-flag {
+    width: 100px;
+    height: 32px;
+    background-color: #39daff;
+    border-radius: 0 20px 20px 0;
+  }
 </style>
